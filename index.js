38fc9770678eebe370871cd0cbef7c810be67e52
James Halliday <mail@substack.net>
2013-12-24 16:36:03 -0800

    passing the reduce test now

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var hasOwn = Object.prototype.hasOwnProperty;
+
 module.exports = function (xs, f, acc) {
-    if (xs.reduce) return xs.reduce(f, acc);
+    var hasAcc = arguments.length >= 3;
+    if (hasAcc && xs.reduce) return xs.reduce(f, acc);
+    if (xs.reduce) return xs.reduce(f);
+    
     for (var i = 0; i < xs.length; i++) {
+        if (!hasAcc) {
+            acc = xs[i];
+            hasAcc = true;
+        }
         if (!hasOwn.call(xs, i)) continue;
         acc = f(acc, xs[i], i);
     }
     return acc;
 };
-
-var hasOwn = Object.prototype.hasOwnProperty;
