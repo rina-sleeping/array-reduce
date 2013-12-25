e833882ea23bd4c005ae400918fa725cda0a5e47
James Halliday <mail@substack.net>
2013-12-24 16:38:08 -0800

    passing the cippled tests

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ module.exports = function (xs, f, acc) {
     if (xs.reduce) return xs.reduce(f);
     
     for (var i = 0; i < xs.length; i++) {
+        if (!hasOwn.call(xs, i)) continue;
         if (!hasAcc) {
             acc = xs[i];
             hasAcc = true;
+            continue;
         }
-        if (!hasOwn.call(xs, i)) continue;
         acc = f(acc, xs[i], i);
     }
     return acc;
--- test/reduce.js
+++ test/reduce.js
@@ -2,7 +2,7 @@ var reduce = require('../');
 var test = require('tape');
 
 test('numeric reduces', function (t) {
-    t.plan(3);
+    t.plan(6);
     
     var xs = [ 1, 2, 3, 4 ];
     t.equal(
@@ -17,4 +17,23 @@ test('numeric reduces', function (t) {
         reduce(xs, function (acc, x) { return acc + x }),
         10
     );
+    
+    var ys = cripple([ 1, 2, 3, 4 ]);
+    t.equal(
+        reduce(ys, function (acc, x) { return acc + x }, 0),
+        10
+    );
+    t.equal(
+        reduce(ys, function (acc, x) { return acc + x }, 100),
+        110
+    );
+    t.equal(
+        reduce(ys, function (acc, x) { return acc + x }),
+        10
+    );
 });
+
+function cripple (xs) {
+    xs.reduce = undefined;
+    return xs;
+}
